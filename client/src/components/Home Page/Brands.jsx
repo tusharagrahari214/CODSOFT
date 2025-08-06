c2b19dfa51f8c530c33bb8aa4af65a90e5741702
tusharagrahari214 <tusharagrahari10@gmail.com>
2025-08-06 11:02:53 +0530

    Initial commit

---
+++ client/src/components/Home Page/Brands.jsx
@@ -0,0 +1,11 @@
+export default function Brands() {
+  return (
+    <div className="bg-white ">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+          Trusted by the world’s most innovative teams
+        </h2>
+      </div>
+    </div>
+  )
+}
